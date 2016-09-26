944a8c99fed3eba45da7f6e69269e15cfbf8f290
Asher Foster <me@asherfoster.com>
2016-09-26 17:56:38 +1300

    Stable 1

--- app.js
+++ app.js
@@ -2,12 +2,37 @@
  * Created by asher on 23/09/16.
  */
 (function(){
-  angular.module( 'Utils', [ 'ngMaterial' ])
+  angular.module( 'Utils', [ 'ngMaterial', 'ngMessages' ])
       .config(function($mdThemingProvider){
         $mdThemingProvider.theme('default')
             .dark();
       })
-      .controller("rootCtrl", function(){
-
+      .filter("urlEncode", function(){
+        return encodeURIComponent;
+      })
+      .controller("rootCtrl", function($scope){
+        $scope.encode = function(){
+          $scope.encoded = encodeURIComponent($scope.plain);
+        };
+        $scope.decode = function(){
+          try{
+            $scope.plain = decodeURIComponent($scope.encoded);
+            $scope.encoder.encoded.$error.invalid = false;
+          }catch(e){
+            $scope.encoder.encoded.$error.invalid = true;
+          }
+        };
+        $scope.cleanCache = function(){
+          $scope.cleaned = false;
+          $scope.cleaning = true;
+          chrome.browsingData.remove({}, {
+            "cache": true
+          }, function(){
+            $scope.$apply(function(){
+              $scope.cleaned = true;
+              $scope.cleaning = false;
+            });
+          });
+        }
       });
 })();
